839fe335998f5b1f24062c89ee37d357d5b10f04
SamTCrow <162122571+SamTCrow@users.noreply.github.com>
2025-03-04 14:23:37 +0100

    feat: improve layout and styling in Risultato component and enhance API response caching

--- components/ui/Risultato.vue
+++ components/ui/Risultato.vue
@@ -11,30 +11,31 @@
 </script>
 
 <template>
-	<div class="flex p-6 text-lg justify-evenly outline outline-1 outline-offset-1 text-center">
-		<div class="gap-4 w-full flex justify-around">
+	<div
+		class="flex gap-2 p-2 md:p-4 md:text-lg text-xs justify-evenly outline outline-1 outline-offset-1 text-center">
+		<div class="gap-4 w-full flex justify-around items-center">
 			<span class="font-bold">{{ partita.puntiCasa }}</span>
 			<span
-				class="w-full grow line-clamp-1"
+				class="w-full grow"
 				:class="
 					gol.casa > gol.ospite
-						? 'scale-105 text-primary-800 dark:text-primary-200 text-lg font-bold'
+						? 'scale-105 text-primary-800 dark:text-primary-200  font-bold'
 						: ''
 				"
 				>{{ partita.nomeCasa?.toUpperCase() }}</span
 			>
 		</div>
-		<div class="gap-4 font-bold flex justify-center">
+		<div class="gap-2 md:gap-4 font-bold flex justify-center items-center">
 			<span class="">{{ gol.casa }}</span>
 			<span class="">-</span>
 			<span class="">{{ gol.ospite }}</span>
 		</div>
-		<div class="gap-4 w-full flex justify-around">
+		<div class="gap-4 w-full flex justify-around items-center">
 			<span
-				class="w-full grow line-clamp-1"
+				class="w-full grow"
 				:class="
 					gol.ospite > gol.casa
-						? 'scale-105 text-primary-800 dark:text-primary-200 text-lg font-bold'
+						? 'scale-105 text-primary-800 dark:text-primary-200  font-bold'
 						: ''
 				"
 				>{{ partita.nomeOspite?.toUpperCase() }}</span
--- layouts/default.vue
+++ layouts/default.vue
@@ -63,6 +63,8 @@
 			position: absolute !important;
 			left: 0 !important;
 			top: 0 !important;
+			font-weight: bold !important;
+			font-size: large !important;
 		}
 	}
 	html {
